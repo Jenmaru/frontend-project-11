dec17c6d3acc3037470eaed9d645465078deb771
Jenmaru <1vanpersonstudio@gmail.com>
2023-11-29 00:06:00 +0300

    step2

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const stylesHandler = 'style-loader';
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     open: true,
